e33870e74269c029259cc3cd667f8ef04763e338
Ivan Urra <ivan.urra.wd@gmail.com>
2021-03-02 13:32:04 +0100

    v-bind

---
+++ app.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp ({
+    data() {
+        return {
+            courseGoal: 'Finish the course',
+            vueLink:'https://vuejs.org/'
+        };
+    }
+});
+
+app.mount('#user-goal');
